be8e6e6d47c496d5b533e8e0cde8761d3d7ff24d
cheeseburgertony <1821955020@qq.com>
2024-06-19 14:01:15 +0800

    layout-静态结构的搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,5 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-  <div>我是Layout</div>
-  <!-- 二级路由出口 -->
-  <RouterView/>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
