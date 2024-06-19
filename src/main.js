0b66c053d55d30cac43d364bacef9b3551141baa
cheeseburgertony <1821955020@qq.com>
2024-06-19 18:17:16 +0800

    home-图片懒加载指令实现

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { useIntersectionObserver } from '@vueuse/core'
+
 const app = createApp(App)
 
 app.use(createPinia())
@@ -13,3 +15,19 @@ app.use(router)
 
 app.mount('#app')
 
+// 自定义指令
+app.directive('img-lazy', {
+  mounted(el, binding) {
+    // el：指令绑定的那个元素 这里是img
+    // binding：binding.value 指令等于号后面的表达式的值 图片url
+    useIntersectionObserver(
+      el,
+      ([{ isIntersecting }]) => {
+        if(isIntersecting){
+          // 如果进入到视口再发送网络请求获取图片
+          el.src = binding.value
+        }
+      }
+    )
+  }
+})
--- src/views/Home/components/HomeHot.vue
+++ src/views/Home/components/HomeHot.vue
@@ -17,7 +17,7 @@ onMounted(() => getHot())
       <ul class="goods-list">
         <li v-for="item in hotList" :key="item.id">
           <RouterLink to="/">
-            <img :src="item.picture" alt="">
+            <img v-img-lazy="item.picture" alt="">
             <p class="name">{{ item.title }}</p>
             <p class="desc">{{ item.alt }}</p>
           </RouterLink>
