b3cceefac48f773246d1dd4fd24a9a054a51175a
cheeseburgertony <1821955020@qq.com>
2024-06-19 19:51:31 +0800

    home-product列表渲染

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,10 +1,28 @@
 import httpInstance from '@/utils/http'
-
-// 获取轮播图数据
+/**
+ * @description: 获取轮播图数据
+ * @param {*}
+ * @return {*}
+ */
 export const getBannerAPI = () => httpInstance.get('/home/banner')
 
-// 获取新鲜好物数据
+/**
+ * @description: 获取新鲜好物数据
+ * @param {*}
+ * @return {*}
+ */
 export const getNewGoodsAPI = () => httpInstance.get('/home/new')
 
-// 获取人气推荐数据
+/**
+ * @description: 获取人气推荐数据
+ * @param {*}
+ * @return {*}
+ */
 export const getHotAPI = () =>httpInstance.get('/home/hot')
+
+/**
+ * @description: 获取产品列表数据
+ * @param {*}
+ * @return {*}
+ */
+export const getGoodsAPI = () =>httpInstance.get('/home/goods')
