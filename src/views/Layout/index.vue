01cf1bef9afad5e0d7a1bf19662f4a1d8848defd
cheeseburgertony <1821955020@qq.com>
2024-06-19 14:57:13 +0800

    layout-吸顶导航交互实现

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,6 +2,7 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixed from './components/LayoutFixed.vue'
 </script>
 
 <template>
@@ -9,4 +10,5 @@ import LayoutFooter from './components/LayoutFooter.vue'
   <LayoutHeader />
   <RouterView />
   <LayoutFooter />
+  <LayoutFixed/>
 </template>
