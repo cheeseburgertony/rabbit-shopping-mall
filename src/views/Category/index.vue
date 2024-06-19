bbd58a94443bd1752e4d5f88f733c173c6a4dd4b
cheeseburgertony <1821955020@qq.com>
2024-06-20 00:17:03 +0800

    一级分类-路由缓存问题解决

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,26 +2,31 @@
 import { getCategoryAPI } from '@/apis/category'
 import { getBannerAPI } from '@/apis/home';
 import { onMounted, ref } from 'vue';
-import { useRoute } from 'vue-router';
+import { onBeforeRouteUpdate, useRoute } from 'vue-router';
 import GoodsItem from '../Home/components/GoodsItem.vue'
 
 // 面包屑导航
 const categoryData = ref({})
 const route = useRoute()
-const getCategory = async () => {
-  const res = await getCategoryAPI(route.params.id)
+const getCategory = async (id = route.params.id) => {
+  const res = await getCategoryAPI(id)
   categoryData.value = res.result
 }
+onMounted(() => getCategory())
+
+// 目标：路由参数变化的时候 可以把分类数据接口重新发送
+// 路由发发送变化的时候 只要求分类数据接口发送，banner不用重新请求发送
+onBeforeRouteUpdate((to) => {
+  // 使用形参to来获取选择的分类的id 使用最新的路由参数请求最新的分类数据
+  getCategory(to.params.id)
+})
 
 const bannerList = ref([])
 const getBanner = async () => {
   const res = await getBannerAPI('2')
   bannerList.value = res.result
 }
-onMounted(() => {
-  getCategory()
-  getBanner()
-})
+onMounted(() => getBanner())
 </script>
 
 <template>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -16,7 +16,9 @@ onMounted(() => {
 <template>
   <LayoutNav />
   <LayoutHeader />
+  <!-- 添加key 破坏复用机制 强制销毁重建 -->
+  <!-- <RouterView :key="$route.fullPath" /> -->
   <RouterView />
   <LayoutFooter />
-  <LayoutFixed/>
+  <LayoutFixed />
 </template>
