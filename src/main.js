e406c09e3497af8d193eae25f8c3ffa503db5c73
cheeseburgertony <1821955020@qq.com>
2024-06-20 19:53:58 +0800

    登录-pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import '@/styles/common.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
@@ -14,7 +15,9 @@ import { componentPlugin } from './components'
 
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentPlugin)
--- src/stores/user.js
+++ src/stores/user.js
@@ -16,4 +16,6 @@ export const useUserStore = defineStore('user', () => {
     userInfo,
     getUserInfo
   }
+},{
+  persist: true
 })
