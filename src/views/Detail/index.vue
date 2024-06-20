d7732206a273ec5a0170df09482c4e60f3adf26d
cheeseburgertony <1821955020@qq.com>
2024-06-20 15:12:27 +0800

    图片预览组件-小图切换大图显示

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,6 +3,7 @@ import { getDetailAPI } from '@/apis/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import DetailHot from './components/DetailHot.vue'
+import ImageView from '@/components/ImageView/index.vue'
 
 // 获取商品详情
 const route = useRoute()
@@ -40,7 +41,7 @@ onMounted(() => getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <ImageView></ImageView>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
