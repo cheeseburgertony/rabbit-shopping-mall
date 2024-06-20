f67aff372ac4d523730005ff73d4eafa9de43e9d
cheeseburgertony <1821955020@qq.com>
2024-06-20 14:16:37 +0800

    详情热榜-基础组件封装和基础数据获取渲染

--- src/apis/detail.js
+++ src/apis/detail.js
@@ -5,4 +5,18 @@ import request from '@/utils/http.js'
  * @return {*}
  */
 export const getDetailAPI = (id) => request.get('/goods', { params: { id } })
- 
+
+/**
+ * 获取热榜商品
+ * @param {Number} id - 商品id
+ * @param {Number} type - 1代表24小时热销榜 2代表周热销榜
+ * @param {Number} limit - 获取个数
+ */
+export const getHotGoodsAPI = ({ id, type, limit = 3 }) =>
+  request.get('/goods/hot', {
+    params: {
+      id,
+      type,
+      limit
+    }
+  })
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -2,6 +2,7 @@
 import { getDetailAPI } from '@/apis/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import DetailHot from './components/DetailHot.vue'
 
 // 获取商品详情
 const route = useRoute()
@@ -122,7 +123,10 @@ onMounted(() => getGoods())
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
-
+              <!-- 24小时热榜 -->
+              <DetailHot></DetailHot>
+              <!-- 周热榜 -->
+              <DetailHot></DetailHot>
             </div>
           </div>
         </div>
