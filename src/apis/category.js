2f63d44ac4c7708047778265b1ac5e96736d3b0f
cheeseburgertony <1821955020@qq.com>
2024-06-20 10:32:21 +0800

    二级分类-面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -1,7 +1,14 @@
 import request from '@/utils/http'
 /**
- * @description: 获取二级分类列表（包括获取当前分类名字）
+ * @description: 获取一级分类列表（包括获取当前分类名字）
  * @param {id}
  * @return {*}
  */
 export const getCategoryAPI = (id) => request.get('/category', { params: { id } })
+
+/**
+ * @description: 获取二级分类列表数据
+ * @param {id}   二级分类id 
+ * @return {*}
+ */
+export const getCategoryFilterAPI = (id) => request.get('/category/sub/filter', { params: { id } })
