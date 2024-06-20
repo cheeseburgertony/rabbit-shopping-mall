ff6282b68983f7e45f7ceac21a4167ea88d61c86
cheeseburgertony <1821955020@qq.com>
2024-06-20 18:14:43 +0800

    登录-表单校验-统一校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -28,9 +28,13 @@ const rules = {
       }
     }
   ]
-
 }
 
+// 表单统一校验 （预校验）
+const formRef = ref()
+const doLogin = async () => {
+  await formRef.value.validate()
+}
 </script>
 
 
@@ -55,7 +59,7 @@ const rules = {
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
+            <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
               <el-form-item prop="account" label="账户">
                 <el-input v-model="form.account" />
               </el-form-item>
@@ -67,7 +71,7 @@ const rules = {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" @click="doLogin" class="subBtn">点击登录</el-button>
             </el-form>
           </div>
         </div>
