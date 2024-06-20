a912794b6c971102fd9c3231c617cb8860228d8e
cheeseburgertony <1821955020@qq.com>
2024-06-20 21:32:26 +0800

    头部购物车-列表渲染和消息框渲染

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useCategoryStore } from '@/stores/category';
+import HeaderCart from './HeaderCart.vue'
 const categoryStore = useCategoryStore()
 </script>
 
@@ -22,7 +23,7 @@ const categoryStore = useCategoryStore()
         <input type="text" placeholder="搜一搜">
       </div>
       <!-- 头部购物车 -->
-      
+      <HeaderCart></HeaderCart>
     </div>
   </header>
 </template>
