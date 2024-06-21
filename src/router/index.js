f098ed37cd719b4e4245c855acfd2bc86ae56566
cheeseburgertony <1821955020@qq.com>
2024-06-22 00:10:05 +0800

    进行优化-路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Layout from '@/views/Layout/index.vue'
-import Login from '@/views/Login/index.vue'
 import Home from '@/views/Home/index.vue'
-import Category from '@/views/Category/index.vue'
-import SubCategory from '@/views/SubCategory/index.vue'
-import Detail from '@/views/Detail/index.vue'
-import CartList from '@/views/CartList/index.vue'
-import Checkout from '@/views/Checkout/index.vue'
-import Pay from '@/views/Pay/index.vue'
-import PayBack from '@/views/Pay/PayBack.vue'
-import Member from '@/views/Member/index.vue'
-import UserInfo from '@/views/Member/components/UserInfo.vue'
-import UserOrder from '@/views/Member/components/UserOrder.vue'
+import Login from '@/views/Login/index.vue'
+
+const Category = () => import('@/views/Category/index.vue')
+const SubCategory = () => import('@/views/SubCategory/index.vue')
+const Detail = () => import('@/views/Detail/index.vue')
+const CartList = () => import('@/views/CartList/index.vue')
+const Checkout = () => import('@/views/Checkout/index.vue')
+const Pay = () => import('@/views/Pay/index.vue')
+const PayBack = () => import('@/views/Pay/PayBack.vue')
+const Member = () => import('@/views/Member/index.vue')
+const UserInfo = () => import('@/views/Member/components/UserInfo.vue')
+const UserOrder = () => import('@/views/Member/components/UserOrder.vue')
 
 const router = createRouter({
+  // 打包时要使用hash模式才不会显示空白 并且在vite.config.js下的defineConfig要配置base: './',变成相对路径
+  // 这样在没部署到服务器前才不会说只有在根路径下才能访问
+  // history: createWebHashHistory(import.meta.env.BASE_URL),
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
@@ -28,8 +32,8 @@ const router = createRouter({
         { path: '/paycallback', component: PayBack },
         {
           path: '/member', component: Member, children: [
-            {path: '', component: UserInfo},
-            {path: 'order', component: UserOrder}
+            { path: '', component: UserInfo },
+            { path: 'order', component: UserOrder }
           ]
         },
       ]
