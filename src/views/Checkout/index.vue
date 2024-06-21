e02260d3c61f3e028e8e4521f0be6f6c01124915
cheeseburgertony <1821955020@qq.com>
2024-06-21 14:34:31 +0800

    地址切换-打开弹框

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -6,10 +6,15 @@ const curAddress = ref({})  // 地址对象
 const getCheckInfo = async () => {
   const res = await getCheckInfoAPI()
   checkInfo.value = res.result
+  // 找到默认地址
   curAddress.value = checkInfo.value.userAddresses.find(item => item.isDefault === 0)
 }
 
 onMounted(() => getCheckInfo())
+
+// 显示弹框
+const showDialog = ref(false)
+
 
 
 </script>
@@ -31,7 +36,7 @@ onMounted(() => getCheckInfo())
               </ul>
             </div>
             <div class="action">
-              <el-button size="large" @click="toggleFlag = true">切换地址</el-button>
+              <el-button size="large" @click="showDialog = true">切换地址</el-button>
               <el-button size="large" @click="addFlag = true">添加地址</el-button>
             </div>
           </div>
@@ -112,6 +117,23 @@ onMounted(() => getCheckInfo())
     </div>
   </div>
   <!-- 切换地址 -->
+  <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
+    <div class="addressWrapper">
+      <div class="text item" v-for="item in checkInfo.userAddresses" :key="item.id">
+        <ul>
+          <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
+          <li><span>联系方式：</span>{{ item.contact }}</li>
+          <li><span>收货地址：</span>{{ item.fullLocation + item.address }}</li>
+        </ul>
+      </div>
+    </div>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button>取消</el-button>
+        <el-button type="primary">确定</el-button>
+      </span>
+    </template>
+  </el-dialog>
   <!-- 添加地址 -->
 </template>
 
