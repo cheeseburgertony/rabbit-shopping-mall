96bfeca73de5fb15e47e517d97993edfd445c91e
cheeseburgertony <1821955020@qq.com>
2024-06-21 18:41:40 +0800

    导航点击跳转优化

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -25,8 +25,8 @@ const confirmHandle = () => {
               </template>
             </el-popconfirm>
           </li>
-          <li><a href="javascript:;">我的订单</a></li>
-          <li><a href="javascript:;">会员中心</a></li>
+          <li><a @click="$router.push('/member/order')" href="javascript:;">我的订单</a></li>
+          <li><a @click="$router.push('/member')" href="javascript:;">会员中心</a></li>
         </template>
         <template v-else>
           <li><a href="javascript:;" @click="$router.push('/login')">请先登录</a></li>
