3ffecb672161068b9a23089afeb43002c6483414
cheeseburgertony <1821955020@qq.com>
2024-06-21 10:37:31 +0800

    本地-列表购物车-基础数据渲染

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
+import CartList from '@/views/CartList/index.vue'
 
 
 
@@ -16,7 +17,8 @@ const router = createRouter({
         { path: '', component: Home },
         { path: '/category/:id', component: Category },
         { path: '/category/sub/:id', component: SubCategory },
-        { path: '/detail/:id', component: Detail }
+        { path: '/detail/:id', component: Detail },
+        { path: '/cartlist', component: CartList }
       ]
     },
     { path: '/login', component: Login }
--- src/views/Layout/components/HeaderCart.vue
+++ src/views/Layout/components/HeaderCart.vue
@@ -35,7 +35,7 @@ const cartStore = useCartStore()
           <p>共 {{ cartStore.totalCount }} 件商品</p>
           <p>&yen; {{ cartStore.totalPrice.toFixed(2) }} </p>
         </div>
-        <el-button size="large" type="primary">去购物车结算</el-button>
+        <el-button size="large" type="primary" @click="$router.push('/cartlist')">去购物车结算</el-button>
       </div>
     </div>
   </div>
