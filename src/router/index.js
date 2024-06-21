12a3a9fb417d8b852394e8fe94fc668e934e6db4
cheeseburgertony <1821955020@qq.com>
2024-06-21 17:15:03 +0800

    会员中心-功能理解以及路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -9,8 +9,9 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
-
-
+import Member from '@/views/Member/index.vue'
+import UserInfo from '@/views/Member/components/UserInfo.vue'
+import UserOrder from '@/views/Member/components/UserOrder.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,7 +25,13 @@ const router = createRouter({
         { path: '/cartlist', component: CartList },
         { path: '/checkout', component: Checkout },
         { path: '/pay', component: Pay },
-        { path: '/paycallback', component: PayBack }
+        { path: '/paycallback', component: PayBack },
+        {
+          path: '/member', component: Member, children: [
+            {path: 'user', component: UserInfo},
+            {path: 'order', component: UserOrder}
+          ]
+        },
       ]
     },
     { path: '/login', component: Login }
